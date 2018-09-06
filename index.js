282318420b601e8c3e887977881e3f2a1e9231f7
Phyllipe Bezerra <pmba@ic.ufal.br>
2018-09-06 13:51:45 -0300

    basic project setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const Key = require('./keys.js');
+
+client.on('ready', () => {
+	console.log('Logado como '+client.user.tag);
+});
+
+client.on('message', msg => {
+	if (msg.content === 'ping') {
+		msg.reply('Pong!');
+	}
+});
+
+client.login(Key.botToken);
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'botToken': 'NDEwNjEzNTg4MTkzOTY4MTI4.DnLqpA.u50a246AJjjpzjCWUzJM3NHreYU'
+}
