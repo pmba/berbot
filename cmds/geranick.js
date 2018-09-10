9fbe645adeeedaeb230c6f1210428d3ee65d45ef
pmba <pmba@ic.ufal.br>
2018-09-10 20:36:05 -0300

    donald trump, nicks commands

---
+++ cmds/geranick.js
@@ -0,0 +1,54 @@
+const Discord = module.require('discord.js');
+const Request = module.require('request');
+const botConfig = module.require('../botConfig.json');
+
+let prefix = botConfig.prefix;
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+module.exports.run = async(client, message, args) => {
+    if (args.length < 2 || (!isNumeric(args[0]) || !isNumeric(args[1]))) {
+        message.channel.send(new Discord.RichEmbed()
+            .setTitle('Erro ao gerar nick :bug:')
+            .setDescription('Informações desse comando:')
+            .addField('Uso', `${prefix}${this.help.name} ${this.help.usage[0]}`)
+            .addField('Numero Mínimo', 2)
+            .addField('Numero Máximo', 40)
+            .setColor('#ff1111')
+        );
+    } else {
+        let minLength = args[0];
+        let maxLength = args[1];
+
+        let options = {
+            method: 'GET',
+            url: `https://uzby.com/api.php?min=${minLength}&max=${maxLength}`,
+            headers: { 
+                'Cache-Control': 'no-cache',
+                'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7' 
+            }
+        };
+
+        Request(options, (err, res, body) => {
+            if (err) console.error(err);
+
+            message.channel.send(new Discord.RichEmbed()
+                .setTitle('Nick Gerado')
+                .setColor('#ffcb11')
+                .addField('Resultado', body)
+            );
+        });
+    }
+    
+}
+
+module.exports.help = {
+    name: "geranick",
+    information: 'Gera um nick dado um número mínimo e máximo de caracteres.\nNúmero mínimo: **2**\nNúmero máximo: **40**',
+    usage: [
+        '<NÚMERO MÍNIMO DE CARACTERES> <NÚMERO MÁXIMO DE CARATERES>'
+    ]
+}
---
+++ cmds/trumpdiz.js
@@ -0,0 +1,45 @@
+const Discord = module.require('discord.js');
+const Request = module.require('request');
+
+module.exports.run = async(client, message, args) => {
+    
+    let person = '';
+    
+    if (!args.length) {
+        person = await encodeURI(message.member.nickname);
+    } else if (message.mentions.users.first()) {
+        person = await encodeURI(message.mentions.users.first().username);
+    }
+
+    let options = await { 
+        method: 'GET',
+        url: `https://api.whatdoestrumpthink.com/api/v1/quotes/personalized?q=${person}`,
+        headers: { 
+            'Cache-Control': 'no-cache',
+            'Accept-Charset': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7' 
+        }
+    }
+
+    await Request(options, (err, res, body) => {
+        if (err) console.error(err);
+
+        body = JSON.parse(body);
+
+        message.channel.send(new Discord.RichEmbed()
+            .setTitle('From: **Donald Trump**')
+            .setDescription(body.message)
+            .setColor('#e5e5e5')
+            .setThumbnail('https://www.gannett-cdn.com/presto/2018/09/09/USAT/59bcb336-865e-47de-baa5-f1c319c056fe-AFP_AFP_18X75M.JPG?width=534&height=401&fit=bounds&auto=webp')
+        );
+    });
+}
+
+module.exports.help = {
+    name: "trumpdiz",
+    information: 'Mostra o que o Donald Trump acha de um determinado usuário.',
+    usage: [
+        '',
+        '<MENÇÃO DO USUÁRIO>'
+    ]
+}
