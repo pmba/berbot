8c11c8438ff55429f932a94b8f3e5b3dd4cff77c
pmba <pmba@ic.ufal.br>
2018-12-17 18:46:48 -0300

    gnome command mentions feature # fix error

--- cmds/gnome.js
+++ cmds/gnome.js
@@ -2,21 +2,26 @@ const Discord = module.require('discord.js');
 const Path = module.require('path');
 
 module.exports.run = async (client, message, args) => {
-    var channel;
 
     if (message.mentions.users.first()) {
-        channel = message.mentions.users.first().voiceChannel.join();
+        message.mentions.users.first().voiceChannel.join()
+        .then(async connection => {
+            const broadcast = client.createVoiceBroadcast();
+            broadcast.playFile(Path.join(__dirname, '../files/gnome.mp3'));
+            for (const connection of client.voiceConnections.values()) {
+                connection.playBroadcast(broadcast);
+            }
+        });
     } else {
-        channel = message.member.voiceChannel.join();
+        message.member.voiceChannel.join()
+        .then(async connection => {
+            const broadcast = client.createVoiceBroadcast();
+            broadcast.playFile(Path.join(__dirname, '../files/gnome.mp3'));
+            for (const connection of client.voiceConnections.values()) {
+                connection.playBroadcast(broadcast);
+            }
+        });
     }
-    
-    channel.then(async connection => {
-        const broadcast = client.createVoiceBroadcast();
-        broadcast.playFile(Path.join(__dirname, '../files/gnome.mp3'));
-        for (const connection of client.voiceConnections.values()) {
-            connection.playBroadcast(broadcast);
-        }
-    });
 
 }
 
