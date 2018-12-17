da452aec66f715d97c50fac17e24888322b30a4d
pmba <pmba@ic.ufal.br>
2018-12-17 18:50:56 -0300

    gnome command mentions feature # nvm

--- cmds/gnome.js
+++ cmds/gnome.js
@@ -2,27 +2,14 @@ const Discord = module.require('discord.js');
 const Path = module.require('path');
 
 module.exports.run = async (client, message, args) => {
-
-    if (message.mentions.users.first()) {
-        message.mentions.users.first().voiceChannel.join()
-        .then(async connection => {
-            const broadcast = client.createVoiceBroadcast();
-            broadcast.playFile(Path.join(__dirname, '../files/gnome.mp3'));
-            for (const connection of client.voiceConnections.values()) {
-                connection.playBroadcast(broadcast);
-            }
-        });
-    } else {
-        message.member.voiceChannel.join()
-        .then(async connection => {
-            const broadcast = client.createVoiceBroadcast();
-            broadcast.playFile(Path.join(__dirname, '../files/gnome.mp3'));
-            for (const connection of client.voiceConnections.values()) {
-                connection.playBroadcast(broadcast);
-            }
-        });
-    }
-
+    message.mentions.users.first().voiceChannel.join()
+    .then(async connection => {
+        const broadcast = client.createVoiceBroadcast();
+        broadcast.playFile(Path.join(__dirname, '../files/gnome.mp3'));
+        for (const connection of client.voiceConnections.values()) {
+            connection.playBroadcast(broadcast);
+        }
+    });
 }
 
 module.exports.help = {
